82cd02d0274c1e7002b68ed0d87426214e169dc8
MelekSenol <90452238+HikmetMelek@users.noreply.github.com>
2021-09-13 17:18:39 +0300

    Add files via upload

---
+++ JavaLessons/src/Sunday_live_lesson_120921/TellMeAboutYourself.java
@@ -0,0 +1,18 @@
+package lessons_EU7.Sunday_live_lesson_120921;
+
+public class TellMeAboutYourself {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("My name is Melek Senol\n");
+		
+		System.out.println("\t I am living in Bursa");
+		System.out.println("I am interested in \"Holistic Life Coaching\"");
+		System.out.println("I want to be a QA :)");
+		
+		
+		
+	}
+
+}
