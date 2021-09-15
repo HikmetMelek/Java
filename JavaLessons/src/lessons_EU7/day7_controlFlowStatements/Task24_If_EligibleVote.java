bef947b1a34d1f631705ca28a519611a4fdb69ff
MelekSenol <90452238+HikmetMelek@users.noreply.github.com>
2021-09-15 20:41:15 +0300

    Add files via upload

---
+++ JavaLessons/src/lessons_EU7/day7_controlFlowStatements/Task24_If_EligibleVote.java
@@ -0,0 +1,12 @@
+package lessons_EU7.day7_controlFlowStatements;
+
+public class Task24_If_EligibleVote {
+
+	public static void main(String[] args) {
+		
+		int age=18;
+			if(age>=18) System.out.println("You are eligible to vote");
+
+	}
+
+}
---
+++ JavaLessons/src/lessons_EU7/day7_controlFlowStatements/Task27_GreetingHours.java
@@ -0,0 +1,17 @@
+package lessons_EU7.day7_controlFlowStatements;
+
+public class Task27_GreetingHours {
+
+	public static void main(String[] args) {
+		
+		int hour= 14;
+				
+			if (hour<12) System.out.println("Good Morning");
+			if (hour>=12 && hour<15) System.out.println("Good Afternoon");
+			if (hour>=15 ) System.out.println("Good Evening");
+			
+			
+			
+	}
+
+}
