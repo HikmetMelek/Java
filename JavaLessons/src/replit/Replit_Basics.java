78c87157d754c90027e2edb329017d4f44ca617c
MelekSenol <90452238+HikmetMelek@users.noreply.github.com>
2021-09-21 13:35:19 +0300

    Add files via upload

---
+++ JavaLessons/src/replit/Replit_Basics.java
@@ -0,0 +1,32 @@
+package replit;
+
+public class Replit_Basics {
+
+	public static void main(String[] args) {
+		int num1 = 2;
+	    double cost1 =5;
+	    System.out.println("num= "+ num1 + "\ncost= "+ cost1);
+	    
+	    System.out.println("===========================");
+	    
+	    String word= "Java";
+	    System.out.println("Today's word of the day is: "+ word);
+	    
+	    System.out.println("===========================");
+	    
+	    int first= -124, second= -500;
+	    System.out.println("first is "+ first + " and second is "+ second);
+	    
+	    System.out.println("===========================");
+	    
+	    String topic1= "Java", topic2= "SQL";
+	    System.out.println("I will learn \""+ topic1+"\" and \""+ topic2+"\" at CybertekSchool.");
+	    
+	    
+	    
+	    
+
+
+	}
+
+}
