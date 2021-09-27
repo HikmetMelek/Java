f01ece6137934733fe4c9e249c464f2bd82a252a
MelekSenol <90452238+HikmetMelek@users.noreply.github.com>
2021-09-27 21:26:51 +0300

    Add files via upload

---
+++ JavaLessons/src/lessons_EU7/day10_controlFlowStatements_Part4/Task_46.java
@@ -0,0 +1,17 @@
+package lessons_EU7.day10_controlFlowStatements_Part4;
+
+public class Task_46 {
+	public static void main(String[] args) {
+		
+		int number=7;
+		System.out.println("Multiplication table for "+number);
+		System.out.println("---------------------------");
+		
+		for (int i=1; i<=10;i++) {
+			System.out.println(number + "*" + i + " = " + (number*i));
+		}
+		
+		
+	}
+
+}
