7c693e025a5df6f5c99dd4f1492945fc0c508723
MelekSenol <90452238+HikmetMelek@users.noreply.github.com>
2021-09-30 15:22:00 +0300

    Add files via upload

---
+++ JavaLessons/src/lessons_EU7/day11_controlFlowStatements_Part5/Task_48_starStep.java
@@ -0,0 +1,17 @@
+package lessons_EU7.day11_controlFlowStatements_Part5;
+
+public class Task_48_starStep {
+
+	public static void main(String[] args) {
+		
+		int rows=7;
+			
+		for(int i=1;i<=rows;i++) {
+						
+			for(int j=1;j<=i;j++) {		
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
