5e6aa4ecdcc6775061b6e574857bfc0293fc9459
MelekSenol <90452238+HikmetMelek@users.noreply.github.com>
2021-10-01 22:36:11 +0300

    Add files via upload

---
+++ JavaLessons/src/day7_controlFlowStatements_Part1/Task30.java
@@ -0,0 +1,16 @@
+package lessons_EU7.day7_controlFlowStatements_Part1;
+
+public class Task30 {
+
+	public static void main(String[] args) {
+		
+		int num=52;
+		//double result;
+		//result= num % 2;  //use modulus, because when you divide a number==> 
+		//even numbers have reminder 0; but odd numbers have remainder 1..
+		if (num%2==0) System.out.println(num+ " is a even number");
+		else System.out.println(num+" is a odd number");
+
+	}
+
+}
