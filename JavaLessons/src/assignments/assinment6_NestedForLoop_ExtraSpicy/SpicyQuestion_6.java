cd64f68175a3ef6f8bbe2cd09619441711bc5ab7
MelekSenol <90452238+HikmetMelek@users.noreply.github.com>
2021-10-05 21:44:50 +0300

    Add files via upload

--- JavaLessons/src/assignments/assinment6_NestedForLoop_ExtraSpicy/SpicyQuestion_6.java
+++ JavaLessons/src/assignments/assinment6_NestedForLoop_ExtraSpicy/SpicyQuestion_6.java
@@ -1,5 +1,7 @@
 package assignments.assinment6_NestedForLoop_ExtraSpicy;
 
+import java.security.Principal;
+
 public class SpicyQuestion_6 {
 
 	public static void main(String[] args) {
@@ -10,24 +12,26 @@ public class SpicyQuestion_6 {
 //		3 7 10
 //		4 8 11 13
 //		5 9 12 14 15
-
-		for (int i=1; i<=5; i++) {
+		
+		int x=5;
+		
+		for (int i=1; i<=x; i++) {
 			
-			for (int j=i; j<=i; j++) {
+			int print=i;
+			
+			System.out.print(print + " ");
+			
+			for (int j=2; j<=i; j++) {	
 				
-				System.out.print(j+" ");
+				int difference= (x+1)-j;
 				
-				}System.out.println();
+				print= print + difference;
+						
+				System.out.print(print +" ");
 					
 			}
-			
-		
-		
-		
-		
-		
-		
-		
+				System.out.println();
+		}
 		
 	}
 
