dc02a9c7dd1b2af15ae1d793e36e0b7444303426
Melek <hmlkshn@gmail.com>
2021-12-17 23:52:57 +0300

    Studies on OOP2

--- JavaLessons/src/replit/OOP/PersonI_MainPerson.java
+++ JavaLessons/src/replit/OOP/PersonI_MainPerson.java
@@ -5,37 +5,16 @@ import java.util.Scanner;
 public class PersonI_MainPerson {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Person person = new Person();
 
-       // System.out.println(person.getFirstName());
-        System.out.println(person.getLastName());
+        Person person = new Person();
+        System.out.println(person.getFirstName());
         System.out.println(person.getAge());
-       // System.out.println(person.toString());
-        person.setFirstName("John");
-        person.setLastName("Doe");
-        person.setAge(44);
-        System.out.println(person.toString() );
+        person.setAge(in.nextInt());
+        System.out.println(person.getAge());
 
         Person person2 = new Person(in.next(), in.next(), in.nextInt());
-        System.out.println(person2.getFirstName()); //"Melek"
-        System.out.println(person2.getLastName());  //"Sun"
-        System.out.println(person2.getAge());       // 42
-        System.out.println(person2.toString());     //"Melek | Sun | 42"
-
-        System.out.println(person.getFirstName());
-        System.out.println(person.getLastName());
-        System.out.println(person.getAge());
-        System.out.println(person.toString());
-//        person.setAge(in.nextInt());
-//        System.out.println(person.getAge());
-//
-//
-//
-//        Person person2 = new Person(in.next(), in.next(), in.nextInt());
-//        System.out.println(person.getLastName());
-//        System.out.println(person.toString());
-
-
+        System.out.println(person2.getLastName());
+        System.out.println(person2.toString());
 
     }
 }
