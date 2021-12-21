1d3612bac385300a824fac1a9773ff2076602ba4
Melek <hmlkshn@gmail.com>
2021-12-21 15:25:46 +0300

    FinalAndHiding

--- JavaLessons/src/replit/arrays/Arrays_3D.java
+++ JavaLessons/src/replit/arrays/Arrays_3D.java
@@ -1,5 +1,26 @@
 package replit.arrays;
 
+import java.util.Arrays;
+
 public class Arrays_3D {
+
+        public static void main(String[] args) {
+            //WRITE YOUR CODE HERE
+
+            int[][][] values= new int[3][2][4];
+
+            for(int a=0; a<4; a++){
+                values[0][0][a]=5;
+                values[0][1][a]=5;
+                values[1][0][a]=7;
+                values[1][1][a]=7;
+                values[2][0][a]=8;
+                values[2][1][a]=30+a;
+
+            }
+
+            //DO NOT CHANGE
+            System.out.println(Arrays.deepToString(values));
+        }
 
 }
